b877b7638aa21640808235e79279deb7ba6066c9
Axel verglas <axelverglas@outlook.com>
2024-04-04 22:30:23 +0200

    Ajouter un lien d'inscription dans le formulaire de connexion

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -27,6 +27,7 @@ import {Icons} from '@/components/icons'
 import Link from 'next/link'
 
 const FormSchema = z.object({
+  username: z.string().min(1, "Nom d'utilisateur requis"),
   email: z.string().email('Email invalide'),
   password: z.string().min(1, 'Mot de passe requis')
 })
@@ -40,27 +41,31 @@ export default function LoginForm() {
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
     setIsLoading(true)
-    const {email, password} = data
-
-    const result = await signIn('credentials', {
-      redirect: false,
-      email,
-      password
+    const {username, email, password} = data
+    const res = await fetch(`http://127.0.0.1:5000/api/v1/user`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({username, email, password})
     })
-
-    if (result?.error) {
+    const result = await res.json()
+    if (res.ok) {
+      setIsLoading(false)
+      toast.success('Inscription réussie')
+      await signIn('credentials', {
+        redirect: true,
+        email,
+        password
+      })
+    } else {
       setIsLoading(false)
       toast.error(result.error)
-    } else {
-      toast.success('Connexion réussie')
-      router.refresh()
     }
   }
 
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
-        <CardTitle className="text-2xl">Se connecter</CardTitle>
+        <CardTitle className="text-2xl">S&apos;inscrire</CardTitle>
       </CardHeader>
       <Form {...form}>
         <form
@@ -68,6 +73,19 @@ export default function LoginForm() {
           className="w-full space-y-4"
         >
           <CardContent className="grid gap-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Nom d&apos;utilisateur</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Votre nom d'utilisateur" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
@@ -102,7 +120,7 @@ export default function LoginForm() {
               {isLoading && (
                 <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
               )}
-              Connexion
+              Inscription
             </Button>
             <div className="text-center text-sm">
               Vous n&apos;avez pas de compte ?{' '}
