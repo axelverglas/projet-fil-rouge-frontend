bdec3d81100357c826b6db178581f72b26d2356c
Axel verglas <axelverglas@outlook.com>
2024-07-11 11:17:27 +0200

    chore: Update NotifNav component to add relative positioning to the button

--- src/components/layout/notif-nav.tsx
+++ src/components/layout/notif-nav.tsx
@@ -74,7 +74,7 @@ export function NotifNav({user}: {user: User}) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="ml-2">
+        <Button variant="ghost" size="icon" className="relative ml-2">
           <BellIcon className="h-5 w-5" />
         </Button>
       </DropdownMenuTrigger>
